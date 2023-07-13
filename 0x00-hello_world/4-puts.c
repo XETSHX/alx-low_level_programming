7e28225cf83cca2840e10ca3350aea37c38621a9
XETSHX <a7med_7isham@yahoo.com>
2023-07-13 07:20:04 -0700

    HELLO WORLD

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - ENTRY POINT
+ *
+ * Description A C program that prints with put function
+ *
+ * Return: ALWAYS 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return(0);
 }
