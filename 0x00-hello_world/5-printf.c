83f386c0534e8b10500f64452ac7ba9d466740da
XETSHX <a7med_7isham@yahoo.com>
2023-07-13 07:27:24 -0700

    HELLO WORLD

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - ENTRY POINT
+ *
+ * Description A C program that prints with printf function
+ *
+ * Return: ALWAYS 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
