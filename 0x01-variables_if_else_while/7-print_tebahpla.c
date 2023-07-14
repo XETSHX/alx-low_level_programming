9acfe4766a317bcd6d688d2423b33e671725a6ae
XETSHX <a7med_7isham@yahoo.com>
2023-07-14 11:51:51 -0700

    if

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,11 +10,13 @@
 
 int main(void)
 {
-	int digit = 0;
+	int digit = 48;
 
-	while (digit <= 9)
+	while (digit <= 102)
 	{
-		putchar(digit + '0');
+		putchar(digit);
+		if (digit == 57)
+			digit += 39;
 		digit++;
 	}
 	putchar('\n');
