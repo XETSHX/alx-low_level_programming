25275bf9c7b1e2574897ff3b88ade6230a6a6675
XETSHX <a7med_7isham@yahoo.com>
2023-07-14 11:17:44 -0700

    if

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desription: print all alphabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
