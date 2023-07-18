432e1c71b091e6098ec1554a1e5f353218e89d49
XETSHX <a7med_7isham@yahoo.com>
2023-07-18 09:44:54 -0700

    if

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,34 @@
 #include "main.h"
-/**
-* main - Entry point
-*
-* Description A C program
-*
-* Return: ALWAYS 0 (Success)
-*/
 
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a string
+ *
+ * Return: 0 (Success)
+*/
 int main(void)
 {
 	char str[] = "_putchar";
 	int ch;
 
 	for (ch = 0; ch < 8; ch++)
-		_putchar(str[ch]);
+	_putchar(str[ch]);
 	_putchar('\n');
 
 	return (0);
+}
+
+/**
+ * _putchar - Custom putchar function
+ * @c: The character to be printed
+ *
+ * Return: On success, returns the character to be printed.
+ * On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c)
+{
+    /* Implementation of the _putchar function */
+    /* ... */
+}
 }
