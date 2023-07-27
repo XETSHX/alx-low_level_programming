5eb845f5de14fc817fef515fca819edab90e1734
XETSHX <a7med_7isham@yahoo.com>
2023-07-27 01:22:14 -0700

    string

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -2,10 +2,10 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
+ * @c: the character to point
  *
- * Return: On Success 1
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: on success 1
+ * on error, -1 is returned, and errno is set appropriately
 **/
 int _putchar(char c)
 {
